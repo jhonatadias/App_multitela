4930dff44e2417ad6e22e781657edb9f65b7877f
Jhonata_Dias <jhonatadias30@gmail.com>
2023-12-13 21:07:17 +0000

    Texte

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,86 @@
 import 'package:flutter/material.dart';
+import 'pages/page2.dart';
+import 'pages/page3.dart';
+import 'pages/app.dart';
+import 'pages/physics_card_drag_demo.dart';
+import 'pages/sunflower.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
+      title: 'Barra de Navegação',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+  //Essa classe está manipulando e configurando
+  // a classe de baixo
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _currentIndex = 0;
+  //Essa classe está sendo manipulada por outra classe
+  // essa manipulação gera a dinamica da mudança das telas
+
+  final List<Widget> _pages = [
+    Page2(),
+    Page3(),
+    Sunflower(),
+    PhysicsCardDragDemo(),
+    App(),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+        title: Text('Barra de Navegação'),
       ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (int index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'app',
+            backgroundColor: Colors.blue,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Página 2',
+            backgroundColor: Colors.blue,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'Página 3',
+            backgroundColor: Colors.blue,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.card_giftcard),
+            label: 'PhysicsCardDragDemo',
+            backgroundColor: Colors.blue,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.local_florist),
+            label: 'Sunflower',
+            backgroundColor: Colors.blue,
+          ),
+        ],
       ),
     );
   }
